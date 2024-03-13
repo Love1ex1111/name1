db4d4d68d1ff41e8006d1d27f720fea4ea7820d4
Love1ex1111 <gitlerlove76@gmail.com>
2024-03-13 18:30:33 +0300

    add kode

--- Xxz.py
+++ Xxz.py
@@ -1,3 +1,19 @@
-print('Hello')
-print('aboba')
-print('sdasd')
+with open('данные.txt', 'r') as file:
+    data = file.readlines()
+
+    students = {}
+
+    for line in data:
+        info = line.strip().split()
+
+        if len(info) >= 3:
+            name = info[0] + ' ' + info[1]
+            grade = info[2]
+            students[name] = grade
+
+user_input = input("Введите имя и фамилию ученика: ")
+
+if user_input in students:
+    print(f"Оценка ученика {user_input}: {students[user_input]}")
+else:
+    print("Данные об этом ученике отсутствуют")
